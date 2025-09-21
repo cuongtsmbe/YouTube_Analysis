fdbd9734982ea37edebbbf5a1cefdfd3fff43129
cuong phan <phanhuucuong05012001@gmail.com>
2025-09-21 22:35:58 +0700

    fix: agent pupperteer

--- src/shared/utils/puppeteer.utils.ts
+++ src/shared/utils/puppeteer.utils.ts
@@ -70,10 +70,16 @@ export class PuppeteerUtils {
 
         try {
           await page.setViewport({ width: 1280, height: 720 });
-          await page.setUserAgent(
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-          );
+          await page.emulate({
+            viewport: {
+              width: 1280,
+              height: 720,
+            },
+            userAgent:
+              "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+          });
 
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
           await page.goto(url, {
             waitUntil: "domcontentloaded",
             timeout: 60000,
